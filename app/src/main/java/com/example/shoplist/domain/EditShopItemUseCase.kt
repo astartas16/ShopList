e809d6c91643211f981ef70f9bb3d6d0a3dd5a1d
stani <astartas16@rambler.ru>
2023-09-21 14:02:21 +0300

    add data layer

--- app/src/main/java/com/example/shoplist/domain/EditShopItemUseCase.kt
+++ app/src/main/java/com/example/shoplist/domain/EditShopItemUseCase.kt
@@ -1,7 +1,7 @@
 package com.example.shoplist.domain
 
 class EditShopItemUseCase(private val shopListRepository: ShopListRepository) {
-    fun editShopItem(shopItem: ShopItem){
-
+    fun editShopItem(shopItem: ShopItem) {
+        shopListRepository.editShopItem(shopItem)
     }
 }
